35e75207614b8fa3c0287d46e02b129ebbc9a45d
deusai <codejesus@protonmail.com>
2017-01-09 19:31:29 -0600

    Celsius to Fahrenheit conversion program from K&R

---
+++ 1-4_exercise.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/*
+ * Write a program to print the corresponding Celsius to Fahrenheit table.
+ * made by Jesus Ricardo Bernal (deusai) on Sunday, January 9, 2017.
+ */
+
+int main()
+{
+	float fahr, celsius;
+	int lower, upper, step;
+	
+	lower = 0;
+	upper = 300;
+	step  = 20;
+
+	printf("Celsius\t\tFahrenheit\n");
+	fahr = lower;
+	while(celsius <= upper) {
+		fahr = (celsius) * (9.0 / 5.0) + 32 ;
+		printf("  %3.0f\t\t%6.1f\n", celsius, fahr);
+		celsius = celsius + step;
+	}
+}
