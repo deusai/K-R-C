8399e3ba760ecd67ede9deee21c3d87d67e13360
deusai <codejesus@protonmail.com>
2017-01-10 19:31:55 -0600

    Refactored the temperature conversion program from K&R to use a for loop instead of a while loop. Also in this version the counter is decreasing from 300 to 0 fahr degrees

---
+++ 1-5_exercise.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/*
+ * Modify the temperature conversion program to print the table in 
+ * reverse order that is from 300 to 0.
+ * made by Jesus Ricardo Bernal (deusai) on Sunday, January 10, 2017.
+ */
+
+int main()
+{
+	int fahr;
+	
+	printf("Fahrenheit\tCelsius\n");
+	for(fahr = 300; fahr >= 0; fahr = fahr - 20){
+		printf("  %3d\t\t%6.1f\n", fahr, (5.0 / 9.0) * (fahr - 32.0));
+	}
+}
