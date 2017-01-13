c75c3d6ed26869362a9ff4dfb7ffa08f0d52c6e7
deusai <codejesus@protonmail.com>
2017-01-12 20:22:07 -0600

    A program that prints the value of the symbolic constant EOF

---
+++ 1-7_exercise.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/*
+ * Write a program to print the value of EOF.
+ * made by Jesus Ricardo Bernal (deusai) on Thursday, January 11, 2017.
+ */
+
+main()
+{
+	printf("%d\n", EOF);
+}
